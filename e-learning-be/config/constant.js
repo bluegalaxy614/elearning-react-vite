5c47341cfc6723738f9986a17eaba1d8d39d78ad
bluegalaxy614 <starandrii170@gmail.com>
2024-12-19 06:51:20 +0200

    final commit

---
+++ e-learning-be/config/constant.js
@@ -0,0 +1,6 @@
+const UNSPLASH_API_KEY = process.env.UNSPLASH_API_KEY;
+const YOUTUBE_API_KEY = process.env.YOUTUBE_API_KEY;
+const OPEN_AI_KEY = process.env.OPEN_AI_KEY;
+exports.UNSPLASH_API_KEY = UNSPLASH_API_KEY;
+exports.YOUTUBE_API_KEY = YOUTUBE_API_KEY;
+exports.OPEN_AI_KEY = OPEN_AI_KEY;
